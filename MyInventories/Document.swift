a691856d439f385d6b4a7550ea6284f6b1af141e
Jacob Miller <jacobmiller22@vt.edu>
2020-07-05 20:05:16 -0400

    initial commit

---
+++ MyInventories/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  MyInventories
+//
+//  Created by Jacob Miller on 7/5/20.
+//  Copyright © 2020 Jacob Miller. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
---
+++ MyInventories/DocumentView.swift
@@ -0,0 +1,27 @@
+//
+//  DocumentView.swift
+//  MyInventories
+//
+//  Created by Jacob Miller on 7/5/20.
+//  Copyright © 2020 Jacob Miller. All rights reserved.
+//
+
+import SwiftUI
+
+struct DocumentView: View {
+    var document: UIDocument
+    var dismiss: () -> Void
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("File Name")
+                    .foregroundColor(.secondary)
+
+                Text(document.fileURL.lastPathComponent)
+            }
+
+            Button("Done", action: dismiss)
+        }
+    }
+}
